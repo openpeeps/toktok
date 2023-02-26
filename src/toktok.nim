19cd68d3a5e8628fb468337d823e3a1b49426590
George Lemon <georgelemon@protonmail.com>
2023-02-26 21:15:39 +0200

    cleanup sample
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/toktok.nim
+++ src/toktok.nim
@@ -6,39 +6,3 @@
 
 import toktok/lexer
 export lexer
-
-when isMainModule:
-  static:
-    Program.settings(
-      uppercase = true,
-      prefix = "Tk_"
-    )
-
-  tokens:
-    Plus      > '+'
-    Minus     > '-'
-    Multi     > '*'
-    Assign    > '=':
-      EQ > '='
-    Div       > '/':
-      BlockComment ? '*' .. "*/"
-      InlineComment ? '/' .. EOL
-    Let       > "let"
-    Const     > "const"
-    Var       > "var"
-
-  let sampleCode = """
-  100.000
-  """
-
-  var lex = Lexer.init(fileContents = sampleCode)
-  if lex.hasError:
-    echo lex.getError
-  else:
-    var curr = lex.getToken()
-    while curr.kind != TK_EOF:
-      if lex.hasError:
-        echo lex.getError
-        break
-      echo curr
-      curr = lex.getToken()
